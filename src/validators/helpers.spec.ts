459b4607f72017a907ee88d7fab6ac3812e085e0
Abiodun Shuaib <abiodun0@users.noreply.github.com>
2016-10-02 12:53:49 -0500

    Test config Cont'd (#7)
    * Test setup and adding typings and tsconfig.json
    
    * finishing setting up tests
    
    * removes global *.js
    
    * readds mistakenly deleted configs
    
    * updated package.json
    
    * modified package.json
    
    * Add tests for smoke component. reconfigured the traspile js directory

---
+++ src/validators/helpers.spec.ts
@@ -0,0 +1,7 @@
+// import * as helper from './helpers';
+
+// describe('test helpers', () => {
+//     it('shold test for exported function', () => {
+//         console.log(helper, 'the helper class')
+//     })
+// })
